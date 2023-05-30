08eae1bab0a38500f95a24fe33b7bd3d55e86740
MOnster-hak <leshagomonovo@mail.ru>
2023-05-30 18:53:38 +0700

    Upgrate local

--- Assets/UI/MainMenuMaterials/ScriptUI/LocalChageScript.cs
+++ Assets/UI/MainMenuMaterials/ScriptUI/LocalChageScript.cs
@@ -5,23 +5,24 @@ using UnityEngine.Localization.Settings;
 
 public class LocalChageScript : MonoBehaviour
 {
-    private bool active = false;
     private void Start(){
-        int ID = PlayerPrefs.GetInt("LocalId", 0);
+        if (!PlayerPrefs.HasKey("LocalId"))
+        PlayerPrefs.SetInt("LocalId", 0);
+        int ID = PlayerPrefs.GetInt("LocalId");
         ChangeLocale(ID);
-        
     }
+    private bool active = false;
     public void ChangeLocale(int localeID){
-        if (active == true)
+        if (active)
           return; 
         StartCoroutine(SetLocale(localeID));
     }
 
-IEnumerator SetLocale(int _localeID){
-    active = true;
-    yield return LocalizationSettings.InitializationOperation;
-    LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID]; 
-    PlayerPrefs.SetInt("LocalId", _localeID);
-    active = false;
-}
+    IEnumerator SetLocale(int _localeID){
+        active = true;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID]; 
+        yield return LocalizationSettings.InitializationOperation;
+        PlayerPrefs.SetInt("LocalId", _localeID);
+        active = false;
+    }
 }
