4e458270bfda1a8fab0a17649bde50732f7581b7
MOnster-hak <leshagomonovo@mail.ru>
2023-05-31 15:10:05 +0700

    Fix localizations

--- Assets/UI/MainMenuMaterials/LocalChageScript.cs
+++ Assets/UI/MainMenuMaterials/LocalChageScript.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 using UnityEngine.Localization.Settings;
 
 public class LocalChageScript : MonoBehaviour
-{
+{   
     private void Start(){
-        int ID = PlayerPrefs.GetInt("LocalId", 0);
-        ChangeLocale(ID);
+
+        if (!PlayerPrefs.HasKey("LocalId"))
+        PlayerPrefs.SetInt("LocalId", 0);
+
+        int id = PlayerPrefs.GetInt("LocalId",0 );
+        ChangeLocale(id);
     }
     private bool active = false;
     public void ChangeLocale(int localeID){
-        if (active == true)
+        if (active)
           return; 
         StartCoroutine(SetLocale(localeID));
+        localeID = PlayerPrefs.GetInt("LocalId");
     }
 
-IEnumerator SetLocale(int _localeID){
-    active = true;
-    LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
-    yield return LocalizationSettings.InitializationOperation;
-    PlayerPrefs.SetInt("LocalId", _localeID);
-    active = false;
-}
+    IEnumerator SetLocale(int _localeID){
+        active = true;
+        print(LocalizationSettings.SelectedLocale); // NE Trogat'!!!!!!!!!!!
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        yield return LocalizationSettings.InitializationOperation;
+        print(LocalizationSettings.SelectedLocale);// NE Trogat'!!!!!!!!!!!
+        PlayerPrefs.SetInt("LocalId", _localeID);
+        active = false;
+    }
 }
