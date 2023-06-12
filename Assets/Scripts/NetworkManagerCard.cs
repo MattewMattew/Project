07b4a8f11a690a3f8b762a3633ddd6c3157e18d0
MOnster-hak <leshagomonovo@mail.ru>
2023-06-12 20:20:56 +0700

    updata

--- Assets/Scripts/NetworkManagerCard.cs
+++ Assets/Scripts/NetworkManagerCard.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using Mirror;
 
 
@@ -9,6 +11,7 @@ using Mirror;
 public enum PlayerSpawnMethod { Random, RoundRobin }
 public enum NetworkManagerMode { Offline, ServerOnly, ClientOnly, Host }
 
+
 [DisallowMultipleComponent]
 [AddComponentMenu("Network/Network Manager")]
 [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-manager")]
@@ -16,6 +19,7 @@ public enum NetworkManagerMode { Offline, ServerOnly, ClientOnly, Host }
 public class NetworkManagerCard : NetworkManager
 {
     public GameObject PrefabCanvas;
+    public TMP_Dropdown DPlayer;
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
         // add player at correct spawn position
@@ -24,8 +28,37 @@ public class NetworkManagerCard : NetworkManager
         GameObject player = Instantiate(playerPrefab, new Vector2(0,0), Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player);
 
-        if (numPlayers == 2)
+        if(DPlayer.value == 0 && numPlayers ==2)
         {
+            
+            /*GiveHandCards(CurrentGame.Pack, EnemyHand);*/
+            GameObject CanvasPref = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Canvas"));
+            NetworkServer.Spawn(CanvasPref);
+        }
+        else if(DPlayer.value == 1 && numPlayers ==4)
+        {
+            
+            /*GiveHandCards(CurrentGame.Pack, EnemyHand);*/
+            GameObject CanvasPref = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Canvas"));
+            NetworkServer.Spawn(CanvasPref);
+        }
+        else if(DPlayer.value == 2 && numPlayers ==5)
+        {
+            
+            /*GiveHandCards(CurrentGame.Pack, EnemyHand);*/
+            GameObject CanvasPref = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Canvas"));
+            NetworkServer.Spawn(CanvasPref);
+        }
+        else if(DPlayer.value == 3 && numPlayers ==6)
+        {
+            
+            /*GiveHandCards(CurrentGame.Pack, EnemyHand);*/
+            GameObject CanvasPref = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Canvas"));
+            NetworkServer.Spawn(CanvasPref);
+        }
+        else if(DPlayer.value == 4 && numPlayers ==7)
+        {
+            
             /*GiveHandCards(CurrentGame.Pack, EnemyHand);*/
             GameObject CanvasPref = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Canvas"));
             NetworkServer.Spawn(CanvasPref);
